c5ff34da910a5e231e0d0ef839b2e7d9469c91e0
Coder242 <79002492+Coder242@users.noreply.github.com>
2021-03-28 18:17:34 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ var wall,bullet;
 var speed,weight;
 var damage;
 var thickness;
+var gamestate = "null";
 
 function setup() {
   createCanvas(1300,400);
@@ -10,12 +11,11 @@ function setup() {
   weight = random(30,52);
   thickness = random(22,83);
 
-  damage = (0.5*weight*speed*speed)/(thickness*thickness*thickness)
+  damage = Math.round((0.5*weight*speed*speed)/(thickness*thickness*thickness))
 
-  bullet = createSprite(50, 200, 50, 50);
+  bullet = createSprite(200, 200, 100, 20);
   wall = createSprite(1200, 200, thickness, height/2);
 
-  bullet.velocityX = speed;
   bullet.shapeColor = "white";
   wall.shapeColor = "yellow";
 
@@ -25,6 +25,8 @@ function draw() {
 
   background("black");  
 
+  bullet.velocityX = speed;
+
     if(bullet.x - wall.x < wall.width/2 + bullet.width/2 &&
        wall.x - bullet.x < wall.width/2 + bullet.width/2 &&
        bullet.y - wall.y < wall.height/2 + bullet.height/2 &&
@@ -32,23 +34,42 @@ function draw() {
        damage < 10)
        {
         
-        bullet.velocityX = 0;
-        wall.shapeColor="green";
+        gamestate = "green";
 
     }
   
-    if(bullet.x - wall.x < wall.width/2 + bullet.width/2 &&
+    else if( (bullet.x - wall.x < wall.width/2 + bullet.width/2 &&
       wall.x - bullet.x < wall.width/2 + bullet.width/2 &&
       bullet.y - wall.y < wall.height/2 + bullet.height/2 &&
-      wall.y - bullet.y < wall.height/2 + bullet.height/2 &&
-       damage === 10 || damage > 10)
+      wall.y - bullet.y < wall.height/2 + bullet.height/2) &&
+       (damage === 10 || damage > 10) )
        {
         
-        bullet.velocityX = 0;
-        wall.shapeColor="red";
+        gamestate = "red";
         
     }
 
+    if(gamestate === "green"){
+
+      bullet.velocityX = 0;
+      bullet.velocityY = 0;
+      wall.shapeColor="green";
+
+    }
+
+    if(gamestate === "red"){
+
+      bullet.velocityX = 0;
+      bullet.velocityY = 0;
+      wall.shapeColor="red";
+      
+    }
+
     drawSprites();
+
+    textSize(20);
+    stroke('white');
+    noFill();
+    text("Damage: "+damage, 100, 50)
 
 }
